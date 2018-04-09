866982b82c1dcd208eee170aab1fd296cc9854f3
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-09 18:10:40 +0100

    WaterBottle test 1 passed.

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    private WaterBottle bottle1;
+
+    @Before
+    public void before() {
+        bottle1 = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, bottle1.getVolume());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
